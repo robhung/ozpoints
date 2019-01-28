b219038092523edb6d16b2a0e331b0825f5f3e27
Robert Hung <rph.hung@gmail.com>
2019-01-28 10:32:33 +0800

    Scaffold App

--- src/App.js
+++ src/App.js
@@ -1,41 +1,76 @@
 import React from "react";
 import injectSheet from "react-jss";
+import { compose, withState } from "recompose";
 
-const App = ({ classes }) => (
-  <div className={classes.App}>
-    <header className={classes.AppHeader}>
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className={classes.AppLink}
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
+import Grid from "@material-ui/core/Grid";
+
+import Filters from "./components/Filters";
+
+const App = ({ classes, filters, updateFilters }) => (
+  <Grid
+    component="main"
+    container
+    direction="column"
+    alignItems="center"
+    className={classes.main}
+  >
+    <Grid component="header" className={classes.header}>
+      <p>OzPoints</p>
+    </Grid>
+    <Filters filters={filters} updateFilters={updateFilters} />
+    <Grid component="section" className={classes.results}>
+      <p>Results</p>
+    </Grid>
+    <Grid component="footer" className={classes.footer}>
+      <p>Robert Hung 2019</p>
+    </Grid>
+  </Grid>
 );
 
 const styles = {
-  App: {
-    textAlign: "center"
-  },
-  AppHeader: {
-    backgroundColor: "#282c34",
+  main: {
     minHeight: "100vh",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "#282c34"
+  },
+  header: {
     fontSize: "calc(10px + 2vmin)",
     color: "white"
   },
-  AppLink: {
-    color: "#61dafb"
+  results: {
+    color: "white"
+  },
+  footer: {
+    marginTop: "auto",
+    color: "white"
   }
 };
 
-export default injectSheet(styles)(App);
+const initialFilters = {
+  cardIssuer: {
+    ANZ: false,
+    CBA: false,
+    NAB: false,
+    Westpac: false
+  },
+  cardType: {
+    VISA: false,
+    Mastercard: false,
+    DinersClub: false,
+    AMEX: false
+  },
+  rewards: {
+    Qantas: false,
+    Velocity: false,
+    Flexible: false
+  },
+  feesWaived: {
+    FirstYearAnnual: false,
+    International: false,
+    Supplemantary: false
+  }
+};
+
+export default compose(
+  injectSheet(styles),
+  withState("filters", "updateFilters", initialFilters)
+)(App);
