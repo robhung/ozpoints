6547ceb093cf4f280a7c752fa6e3f931f556b671
Robert Hung <rph.hung@gmail.com>
2019-02-02 21:08:07 +0800

    Add About

--- src/App.js
+++ src/App.js
@@ -21,23 +21,38 @@ const App = ({ classes, filters, updateFilters }) => (
     </Grid>
     <Filters filters={filters} updateFilters={updateFilters} />
     <ResultsTable filters={filters} updateFilters={updateFilters} />
+    <Grid component="section" className={classes.about}>
+      <p>
+        This site was made for comparing credit cards in Australia which offer a
+        sign-up bonus.
+      </p>
+      <p>
+        Data relevant to optimise rewards bonus has been organised in an easily
+        digestible format.
+      </p>
+    </Grid>
     <Grid component="footer" className={classes.footer}>
       <p>Robert Hung 2019</p>
     </Grid>
   </Grid>
 );
 
-const styles = {
+const styles = theme => ({
   main: {
     minHeight: "100vh"
   },
   header: {
     fontSize: "calc(10px + 2vmin)"
   },
+  about: {
+    marginTop: "auto",
+    marginLeft: "25%",
+    marginRight: "25%"
+  },
   footer: {
-    marginTop: "auto"
+    marginBottom: theme.spacing.unit * 3
   }
-};
+});
 
 const initialFilters = {
   issuer: {
