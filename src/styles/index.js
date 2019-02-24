e220df692a5093c6d6c2d08f96d8dc039a58d075
Robert Hung <rph.hung@gmail.com>
2019-02-24 19:06:33 +0800

    Create OzBargain theme

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,17 +1,27 @@
 import { createMuiTheme } from '@material-ui/core/styles';
-import orange from '@material-ui/core/colors/orange';
 import grey from '@material-ui/core/colors/grey';
 import red from '@material-ui/core/colors/red';
 
 // All the following keys are optional.
 // We try our best to provide a great default value.
 
-// eslint-disable-next-line no-unused-vars
-const ozbargainOrange = '#CC6703';
+const ozBargainTheme = {
+  lighter: '#fffae8',
+  light: '#fff0c0',
+  mainLighest: '#f7a859',
+  mainLighter: '#e69c53',
+  mainLight: '#d9934e',
+  main: '#cd6702',
+  dark: '#8f4801'
+};
 
 const theme = createMuiTheme({
   palette: {
-    primary: orange,
+    common: {
+      black: '#333',
+      white: 'F8F8FF'
+    },
+    primary: ozBargainTheme,
     secondary: grey,
     error: red,
     // Used by `getContrastText()` to maximize the contrast between the background and
@@ -21,6 +31,9 @@ const theme = createMuiTheme({
     // two indexis within its tonal palette.
     // E.g., shift from Red 500 to Red 300 or Red 700.
     tonalOffset: 0.2
+  },
+  typography: {
+    useNextVariants: true
   }
 });
 
